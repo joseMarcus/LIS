f689a2c0a87c4d0995a56873817e3f4de6a89779
joseMarcus <jose.marcus@academico.ifpb.edu.br>
2022-02-16 21:09:05 -0300

    comentar codigo

--- CafeAppLis/js/cardapio.js
+++ CafeAppLis/js/cardapio.js
@@ -1,23 +1,35 @@
+// Importar PARA "itens" DO arquivo Javascript './model/dataset.js'
 import itens from './model/dataset.js';
+// Importar PARA "foodsModel" DO arquivo Javascript './model/foods.js'
 import foodsModel from './model/foods.js';
 
+// Uma função do food.js(load) para trabalhar um modelo do food.js(foodsModel) a partir do conjunto de itens em dataset.js(itens)
 foodsModel.load(itens);
+//Declaração da variável(foods) limitada no escopo(let) que recebe o resultado de uma outra função do food.js(readAll)
 let foods = foodsModel.readAll();
 
+//Uma função criada neste script(initFoodsCard)
 function initFoodsCard () {
   
+  //Um laço(for) criando uma variável chamada "item" para trabalhar a variável "foods", declarada anteriormente
   for (let item of foods) {
 
+    //Uma variável constante chamada "view" que recebe uma outra função no mesmo script que trabalha a variável "item" deste laço
     const view = createFoodCardItem(item);
-  
+
     //let itensCardapio = document.querySelector('.itens-cardapio');
+
+    //Declaração da variável(itensCardapio) limitada no escopo(let) que recebe a referência do elemento através do ID("itens-cardapio").
     let itensCardapio = document.getElementById("itens-cardapio");
+    //Analisa o texto e insere os nós dentro do elemento após seu último filho(beforeend), como parâmetro a variável "view"
     itensCardapio.insertAdjacentHTML('beforeend', view);
   }
 }
 
+//Uma função criada neste script(createFoodCardItem)
 function createFoodCardItem (item) {
 
+    //Uma variável constante chamada "view" que recebe uma string que no momento gera uma imagem, nome e descrição diferente importado do dataset.js, apesar da declaração da variável não "bater"
     const view = `<div class="col-3 card my-1 mx-1 py-1">
                     <img src="${item.imagem}" class="card-img-top" alt="...">
   
@@ -27,25 +39,36 @@ function createFoodCardItem (item) {
                       <a href="#" class="btn btn-primary">Comprar</a>
                     </div>
                   </div>`;
-
+    //Retornar a cada uso a variável "view"
     return view;
 }
 
 
 // Captar o evento de submissão do formulário e adicionar o item no cartão (card).
 // const foodForm = document.querySelector('#foodForm');
+
+//Declaração da variável constante(foodForm) que recebe a referência do elemento através do ID("foodForm").
 const foodForm = document.getElementById("foodForm");
 
+//Função com base no módulo "foodForm" com o parâmetro de entrada chamado "event"
 foodForm.onsubmit = function (event) {
   // Previnir que o modal fique abrindo e fechando em loop.
+
+  //Função com base no parâmetro "event"
   event.preventDefault();
 
+  //Declaração da variável(newFood) limitada no escopo(let) que recebe um objeto chamado "Object" como propriedade pra função "fromEntries", com parâmetros de uma nova variável além de outro objeto  que recebe uma variável constante declarada anteriormente
   let newFood = Object.fromEntries(new FormData(foodForm));
+  //Função deste escopo com script do food.js para trabalhar dados da variável anterior como parâmetro
   foodsModel.create(newFood);
 
+  //Declaração da variável constante(foodCard) que recebe a função anterior a esta no script mas que trabalha com os novos dados gerados anteriormente advinfos do parâmetro "newFood"
   const foodCard = createFoodCardItem(newFood);
+  
+  //Declaração da variável NOVAMENTE (itensCardapio) limitada no escopo(let) que recebe a referência do elemento através do ID("itens-cardapio").
   let itensCardapio = document.getElementById("itens-cardapio");
+  //Analisa o texto e insere os nós dentro do elemento após seu último filho(beforeend), MAS agora com o parâmetro da variável "foodCard"
   itensCardapio.insertAdjacentHTML('beforeend', foodCard);
 }
-
+//"Chamando" a função
 initFoodsCard();
--- CafeAppLis/js/model/dataset.js
+++ CafeAppLis/js/model/dataset.js
@@ -1,32 +1,49 @@
+// Array/Lista declarada como a variável constante de valor fixo chamada "dataset" com vários jsons e várias chaves e valores em cada conjunto
 const dataset = [
   {
+    //Chave e valor(id:1)
     id: 1,
+    //Chave e valor(Hambúrguer)
     name: 'Hambúrguer',
+    //Chave e valor(imagem com endereçamento relativo, mesmo que não a tenha)
     image: 'images/hamburguer.jpg',
+    //Chave e valor(legenda/descrição)
     description:
       'Um hambúrguer é um sanduíche que consiste em um ou mais hambúrgueres cozidos de carne moída, geralmente carne bovina, colocados dentro de um pão ou pão fatiado. O hambúrguer pode ser frito, grelhado, defumado ou grelhado',
   },
   {
+    //Chave e valor(id:2)
     id: 2,
+    //Chave e valor(Sanduíche)
     name: 'Sanduíche',
+    //Chave e valor(imagem com endereçamento relativo, mesmo que não a tenha)
     image: 'images/sanduiche.jpg',
+    //Chave e valor(legenda/descrição)
     description:
       'O sanduíche (do inglês sandwich), também popularmente chamado de sanduba (português brasileiro) ou sandes (português europeu), é um tipo de alimento que consiste em duas fatias de um pão inteiro, entre as quais é colocada carne, queijo ou outro tipo de alimento.Os sanduíches são, habitualmente, consumidos ao lanche ou como uma refeição rápida, durante o almoço ou o jantar.',
   },
   {
+    //Chave e valor(id:3)
     id: 3,
+    //Chave e valor(Café)
     name: 'Café',
+    //Chave e valor(imagem com endereçamento relativo, mesmo que não a tenha)
     image: 'images/cafe.jpg',
+    //Chave e valor(legenda/descrição)
     description:
       'O café é uma bebida produzida a partir dos grãos torrados do fruto do cafeeiro. É servido tradicionalmente quente, mas também pode ser consumido gelado. O café é um estimulante, por possuir cafeína — geralmente 80 a 140 mg para cada 207 ml dependendo do método de preparação.',
   },
-  {
+  {    
+    //Chave e valor(id:4)
     id: 4,
+    //Chave e valor(Suco)
     name: 'Suco',
+    //Chave e valor(imagem com endereçamento relativo, mesmo que não a tenha)
     image: 'images/suco.jpg',
+    //Chave e valor(legenda/descrição)
     description:
       'Um suco (português brasileiro) ou sumo (português europeu) é uma bebida produzida do líquido extraído de frutos. Além dos frutos, sucos também podem ser obtidos pelo processamento de outras partes dos vegetais, tais como folhas, talos ou raízes.',
   },
 ];
-
+//Exportador de dados para os elementos HTML através do objeto "dataset"
 export default dataset;
